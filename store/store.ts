7dc96e8141eb67bda998572025f1328156a876e8
Nur Mohammad Apu <mdapu2003@gmail.com>
2025-05-08 13:09:09 +0600

    Remove fonts.ts and update store configuration

--- store/store.ts
+++ store/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import tasksReducer from "./features/tasks/tasksSlice"
-import filtersReducer from "./features/filters/filtersSlice"
 import authReducer from "./features/auth/authSlice"
 import { useDispatch } from "react-redux"
 
 export const store = configureStore({
   reducer: {
     tasks: tasksReducer,
-    filters: filtersReducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
